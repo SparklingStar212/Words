612d6b16d88a955663f226e7d54ecd46de379646
SparklingStar212 <deborahalabi49@gmail.com>
2026-09-07 12:30:32 +0100

    Remove unnecessary React import in InstallPrompt component

--- frontend/src/components/InstallPrompt.tsx
+++ frontend/src/components/InstallPrompt.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 // Extend the WindowEventMap for the PWA install event
 interface BeforeInstallPromptEvent extends Event {
